54e751bc127365093b475beb2ab673ad3aabb4e0
maxgmr <mxzmxz120@gmail.com>
2024-05-11 11:26:44 -0700

    Refactor load_image as ImgsimImage method, change ImgsimImage to rgba8

--- src/data/imgsim_image.rs
+++ src/data/imgsim_image.rs
@@ -1,23 +1,73 @@
 #[warn(missing_docs)]
-use image::DynamicImage;
+use image::{ImageError, RgbaImage};
+use std::path::{Path, PathBuf};
 
+/// An [image::RgbaImage] with metadata, similarity factors, and clusters.
 pub struct ImgsimImage {
     name: String,
-    dynamic_image: DynamicImage,
+    path: PathBuf,
+    rgba_image: RgbaImage,
 }
 impl ImgsimImage {
-    pub fn new(name: String, dynamic_image: DynamicImage) -> ImgsimImage {
-        ImgsimImage {
-            name,
-            dynamic_image,
+    /// Creates a new [ImgsimImage] by loading the image at the given file path.
+    ///
+    /// Returns [Option::Some]\([ImgsimImage]\) if an image at that path exists and that image loads successfully.
+    ///
+    /// Returns [Option::None](std::option::Option) and prints a warning message if an image exists, but there was a problem reading the image.
+    ///
+    /// Returns [Option::None](std::option::Option) without printing a message if the file path does not link to an image.
+    pub fn new(image_path: &Path) -> Option<ImgsimImage> {
+        match (image_path.file_name(), image::open(image_path)) {
+            (Some(file_name), Ok(image)) => {
+                if let Some(name) = file_name.to_str() {
+                    Some(ImgsimImage {
+                        name: String::from(name),
+                        path: PathBuf::from(image_path),
+                        rgba_image: image.to_rgba8(),
+                    })
+                } else {
+                    eprintln!(
+                        "Warning: Could not parse file name at {}",
+                        image_path.to_str().unwrap_or("unknown directory")
+                    );
+                    None
+                }
+            }
+            (_, Err(ImageError::Unsupported(_))) => None,
+            (Some(file_name), Err(image_error)) => {
+                eprintln!(
+                    "Warning @ {}: {}",
+                    file_name.to_str().unwrap_or("unknown file"),
+                    image_error.to_string()
+                );
+                None
+            }
+            (_, Err(image_error)) => {
+                eprintln!("Warning: {}", image_error.to_string());
+                None
+            }
+            (_, Ok(_)) => {
+                eprintln!(
+                    "Warning: Could not parse file name at {}, so cannot use valid image",
+                    image_path.to_str().unwrap_or("unknown directory")
+                );
+                None
+            }
         }
     }
 
+    /// Returns the name of the image.
     pub fn name(&self) -> &str {
         &self.name
     }
 
-    pub fn dynamic_image(&self) -> &DynamicImage {
-        &self.dynamic_image
+    /// Returns a reference to the path of the image as [std::path::PathBuf].
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    /// Returns a reference to the image as an [image::RgbaImage].
+    pub fn rgba_image(&self) -> &RgbaImage {
+        &self.rgba_image
     }
 }
--- src/persistence/load_images.rs
+++ src/persistence/load_images.rs
@@ -1,5 +1,3 @@
-use image::error::ImageError;
-
 use std::fs;
 use std::path::{Path, PathBuf};
 
@@ -14,7 +12,7 @@ pub fn load_images(input_dir_path: &Path) -> Result<Vec<ImgsimImage>, Persistenc
             entry.ok().and_then(|ok_entry| {
                 let path = ok_entry.path();
                 if path.is_file() {
-                    load_image(&path)
+                    ImgsimImage::new(&path)
                 } else {
                     None
                 }
@@ -27,41 +25,5 @@ pub fn load_images(input_dir_path: &Path) -> Result<Vec<ImgsimImage>, Persistenc
         ))))
     } else {
         return Ok(images);
-    }
-}
-
-fn load_image(image_path: &Path) -> Option<ImgsimImage> {
-    match (image_path.file_name(), image::open(image_path)) {
-        (Some(file_name), Ok(image)) => {
-            if let Some(name) = file_name.to_str() {
-                Some(ImgsimImage::new(String::from(name), image))
-            } else {
-                eprintln!(
-                    "Warning: Could not parse file name at {}",
-                    image_path.to_str().unwrap_or("unknown directory")
-                );
-                None
-            }
-        }
-        (_, Err(ImageError::Unsupported(_))) => None,
-        (Some(file_name), Err(image_error)) => {
-            eprintln!(
-                "Warning @ {}: {}",
-                file_name.to_str().unwrap_or("unknown file"),
-                image_error.to_string()
-            );
-            None
-        }
-        (_, Err(image_error)) => {
-            eprintln!("Warning: {}", image_error.to_string());
-            None
-        }
-        (_, Ok(_)) => {
-            eprintln!(
-                "Warning: Could not parse file name at {}, so cannot use valid image",
-                image_path.to_str().unwrap_or("unknown directory")
-            );
-            None
-        }
     }
 }
