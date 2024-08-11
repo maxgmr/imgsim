12da176315a3e914bf33c5f70e026114f8816a93
maxgmr <mxzmxz120@gmail.com>
2024-08-11 14:31:30 -0700

    add git support to build

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
-use vergen::{BuildBuilder, CargoBuilder, Emitter};
+use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let build = BuildBuilder::all_build()?;
     let cargo = CargoBuilder::all_cargo()?;
+    let gitcl = GixBuilder::all_git()?;
 
     Emitter::default()
         .add_instructions(&build)?
         .add_instructions(&cargo)?
+        .add_instructions(&gitcl)?
         .emit()?;
 
     Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,11 @@
 //! General utilities used by `imgsim`.
 use std::env;
 
-const VERSION_MESSAGE: &str = concat!();
+const VERSION_MESSAGE: &str = concat!(
+    env!("CARGO_PKG_VERSION"),
+    " -",
+    env!("VERGEN_GIT_DESCRIBE"),
+    " (",
+    env!("VERGEN_BUILD_DATE"),
+    ")"
+);
